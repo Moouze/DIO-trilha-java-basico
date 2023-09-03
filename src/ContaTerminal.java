39f5d88ab8b34b9312523c7168d4484e497b8a65
Lucas <lucas_dev@outlook.com.br>
2023-09-03 16:38:50 -0300

    Solicitando os dados para o cliente

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -5,6 +5,18 @@ public class ContaTerminal {
 
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("Prezado Cliente, digite o numero da sua conta: ");
+        int numero = sc.nextInt();
+
+        System.out.println("Prezado Cliente, digite o numero da sua agencia: ");
+        String agencia = sc.nextLine();
+
+        System.out.println("Prezado Cliente, digite seu nome: ");
+        String nome = sc.nextLine();
+
+        System.out.println("Prezado Cliente, digite o saldo da sua conta: ");
+        double saldo = sc.nextDouble();
+
 
 
     }
