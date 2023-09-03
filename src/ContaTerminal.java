9b8e6ed53a20d72d6dc81e29010e2d35c7803cec
Lucas <lucas_dev@outlook.com.br>
2023-09-03 16:47:24 -0300

    Imprimindo os dados coletados pelo usuario e mostrando no terminal da IDE

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -6,18 +6,19 @@ public class ContaTerminal {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Prezado Cliente, digite o numero da sua conta: ");
-        int numero = sc.nextInt();
+        int conta = sc.nextInt();
 
         System.out.println("Prezado Cliente, digite o numero da sua agencia: ");
-        String agencia = sc.nextLine();
+        String agencia = sc.next();
 
         System.out.println("Prezado Cliente, digite seu nome: ");
-        String nome = sc.nextLine();
+        String nome = sc.next();
 
         System.out.println("Prezado Cliente, digite o saldo da sua conta: ");
         double saldo = sc.nextDouble();
 
-
+        System.out.println("Sr(a) " + nome + ", Seja bem vindo a nosso banco, o numero da sua conta é " + conta + ", o numero da sua agencia é " + agencia +
+                " e o saldo é "+ saldo );
 
     }
 }
